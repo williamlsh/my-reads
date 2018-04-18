69b701b1c6cc98bd061eb2b28dd7a360eb829d1e
Violet Moon <11584947+Violetripple@users.noreply.github.com>
2018-04-18 22:03:41 +0800

    remove imported icon files from index.js, no need

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import "./icons/add.svg";
-import "./icons/arrow-back.svg";
-import "./icons/arrow-drop-down.svg";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
